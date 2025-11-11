7867d2471e56cfd712dc090a6e79bb2b5384e416
Md Abdur Rahim <dev.abdurrahim@outlook.com>
2025-11-12 00:05:30 +0600

    start javascript bootcam'

---
+++ prerequsite/index.js
@@ -0,0 +1,5 @@
+console.log("Hello java script");
+
+console.log("Hello under world");
+
+console.log("Shryaiscoding School");
---
+++ variableDeclaration/script.js
@@ -0,0 +1,9 @@
+// var, let, const - Line by line comparison
+
+var a = 12;
+var username = "rahim";
+let lastName = "Kalam";
+const yourCity = "Dhaka";
+
+var a; //declare
+const payment = "30"; //declare and initialize
