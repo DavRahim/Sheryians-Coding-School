fd87a0b89ff486fb301732c770f10bf9dd8e89b1
Md Abdur Rahim <dev.abdurrahim@outlook.com>
2025-12-09 20:38:36 +0600

    game project end

---
+++ Advanced_JavaScript_Part-3/script.js
@@ -0,0 +1,37 @@
+console.log("hello advance");
+
+// TODO:TODO: Scope, Execution Context, Closures.
+
+//TODO: 1.Scope
+// Functional Scope, Global scope, and Block scope.
+
+
+//TODO:TODO: The This keyword 
+
+let obj = {
+    name: "harsh"
+}
+
+function abcd() {
+    console.log(this);
+}
+abcd.call(obj);
+const userManger = {
+    user: [],
+    init: function () {
+
+    },
+    addUser: function () {
+
+    }
+}
+
+
+//TODO:TODO: Object-Oriented Javascript;
+
+
+//TODO:TODO: Callbacks, Promises & Async/Await;
+
+
+// TODO:TODO: Fetch API + HTTP Basics;
+
